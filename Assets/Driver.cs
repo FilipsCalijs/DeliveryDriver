bd654de745f4fd597f7b0a0536ef36a2340bf2c4
Fil <88441203+FilipsCalijs@users.noreply.github.com>
2024-08-11 14:38:44 +0300

    add transform.Translate and incoductin varibles

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    // Объявляем переменные на уровне класса
+    float steerSpeed = 1f;
+    float moveSpeed = 0.1f;
+
     // Start is called before the first frame update
     void Start()
     {
- transform.Rotate(0, 0, 45); 
+        // Можно инициализировать переменные в Start(), но это необязательно, так как они уже инициализированы выше
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Теперь переменные доступны и здесь
+        transform.Rotate(0, 0, steerSpeed);
+        transform.Translate(0, moveSpeed, 0);
     }
 }
