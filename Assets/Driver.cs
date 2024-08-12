ec8707f303f6fc660e817976e7e08c073ba7dbe1
Fil <88441203+FilipsCalijs@users.noreply.github.com>
2024-08-12 23:21:26 +0300

    Using Input.GetAxis()

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Driver : MonoBehaviour
 {
     // Объявляем переменные на уровне класса
-    float steerSpeed = 1f;
-    float moveSpeed = 0.1f;
+    [SerializeField] float steerSpeed = 1f;
+    [SerializeField] float moveSpeed = 0.1f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,10 +15,13 @@ public class Driver : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+      void Update()
     {
-        // Теперь переменные доступны и здесь
-        transform.Rotate(0, 0, steerSpeed);
-        transform.Translate(0, moveSpeed, 0);
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
     }
+
+
 }
