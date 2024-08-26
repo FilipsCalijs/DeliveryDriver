ff7c6a1b90c98824911249f68c495fc952adfe81
FilipsCalijs <filips.calijs@gmail.com>
2024-08-26 22:30:40 +0300

    Use GetComponent

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     [SerializeField] float destroyDelay = 0.5f;
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
     bool hasPackage;
-    
+    SpriteRenderer spriteRenderer;
+
+    void Start() 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();    
+    }
+
     void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log("Ouch!");
@@ -14,10 +22,11 @@ public class Delivery : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other) 
     {
-          if (other.tag == "Package" && !hasPackage)
+        if (other.tag == "Package" && !hasPackage)
         {
             Debug.Log("Package picked up");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(other.gameObject, destroyDelay);
         }
 
@@ -25,8 +34,7 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package Delivered");
             hasPackage = false;
-            Destroy(other.gameObject, destroyDelay);
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
-
