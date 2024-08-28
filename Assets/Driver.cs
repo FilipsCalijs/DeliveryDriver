b15d96565214ec793cae9764108e686784aa26e0
FilipsCalijs <filips.calijs@gmail.com>
2024-08-28 23:45:19 +0300

    Boosts & Bumps

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,24 +4,30 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    // Объявляем переменные на уровне класса
     [SerializeField] float steerSpeed = 1f;
-    [SerializeField] float moveSpeed = 0.1f;
+    [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float boostSpeed = 30f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Можно инициализировать переменные в Start(), но это необязательно, так как они уже инициализированы выше
-    }
-
-    // Update is called once per frame
-      void Update()
+    void Update()
     {
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
-        float moveAmount = Input.GetAxis("Vertical") * moveSpeed  * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
     }
 
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        moveSpeed = slowSpeed;    
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+        }    
+    }
 
 }
